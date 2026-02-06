73ee24ef2dc61a6eac93b5571145149d8a8c194d
Jitesh Singh <jiteshs101@gmail.com>
2026-02-06 15:36:27 +0530

    Header Minimalized

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 export default function Header() {
   return (
     <div className="space-y-1">
-      <h1 className="text-3xl font-semibold tracking-tight">
-        🚨Fraud Detection 💳
+      <h1 className="text-3xl font-bold tracking-tight">
+        💶 Fraud Detection
       </h1>
       <p className="text-gray-500">
-        Real-time fraud detection powered by Kafka Streams
+        Real-time fraud detection powered by Kafka Stream
       </p>
     </div>
   );
