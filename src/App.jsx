f7980a2cd01b53f8796bc592f607793a3bfe12d2
Jitesh Singh <jiteshs101@gmail.com>
2026-02-13 14:03:03 +0530

    Vercel Analytics Configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next"
 import Header from "./components/Header";
 import StreamStatus from "./components/StreamStatus";
 import FraudAlerts from "./components/FraudAlerts";
@@ -8,16 +9,15 @@ export default function App() {
     <div className="min-h-screen bg-[#fafafa] px-6 py-10">
       <div className="max-w-6xl mx-auto space-y-10">
         <Header />
-
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           <div className="space-y-6">
             <StreamStatus />
             <TransactionForm />
           </div>
-
           <FraudAlerts />
         </div>
       </div>
+      <Analytics />
     </div>
   );
 }
