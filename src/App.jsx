4fa7f7014a5d0a41bec1489aff57c3940d6e55c1
Jitesh Singh <jiteshs101@gmail.com>
2026-02-13 14:06:29 +0530

    Vercel Speed Insights Configured

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import StreamStatus from "./components/StreamStatus";
 import FraudAlerts from "./components/FraudAlerts";
 import TransactionForm from "./components/TransactionForm";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function App() {
   return (
@@ -18,6 +19,7 @@ export default function App() {
         </div>
       </div>
       <Analytics />
+      <SpeedInsights />
     </div>
   );
 }
